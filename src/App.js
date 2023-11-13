14a0c5bfe46e0479617b7363c030e86c2fc7bd7e
N4CHT0 <hannessinaga77@gmail.com>
2023-11-13 20:29:56 +0700

    [BAB5]Navigation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
-import * as React from 'react'
-import {Home,Profile,Training} from './pages';
-export default function App() {
-  return <Home />;
-} 
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native';
+import Router from './router';
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Router/>
+  </NavigationContainer>
+  )
+}
+export default App
+const styles = StyleSheet.create({})
---
+++ src/router/index.js
@@ -0,0 +1,57 @@
+import { StyleSheet, Text, View } from 'react-native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import {  Home2, Avalanche, Profile2User } from 'iconsax-react-native';
+
+import React from 'react'
+import { Home,Profile,Training } from '../pages';
+const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+const MainApp = () => {
+  return (
+    <Tab.Navigator>
+        <Tab.Screen name="Home" component={Home} options={{
+          tabBarLabel: 'Home',
+          tabBarIcon: ({focused, color}) => (
+            <Home2
+              color={color}
+              variant={focused ? 'Bold' : 'Linear'}
+              size={24}
+            />
+          ),
+          headerShown: false,
+        }} />
+        <Tab.Screen name="Training" component={Training} options={{
+          tabBarLabel: 'Training',
+          tabBarIcon: ({focused, color}) => (
+            <Avalanche
+              color={color}
+              variant={focused ? 'Bold' : 'Linear'}
+              size={24}
+            />
+          ),
+          headerShown: false,
+        }}/>
+        <Tab.Screen name="Profile" component={Profile} options={{
+          tabBarLabel: 'Profile',
+          tabBarIcon: ({focused, color}) => (
+            <Profile2User
+              color={color}
+              variant={focused ? 'Bold' : 'Linear'}
+              size={24}
+            />
+          ),
+          headerShown: false,
+        }}/>
+      </Tab.Navigator>
+  )
+}
+const Router = () => {
+  return (
+    <Stack.Navigator initialRouteName="Splash">
+      <Stack.Screen name="MainApp" component={MainApp} options={{ headerShown: false }} />
+    </Stack.Navigator>
+  )
+}
+export default Router
+const styles = StyleSheet.create({})
